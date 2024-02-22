ed93657e8f1d19321a825cc7ff20b7865d4ae27b
Mirsaid Abdurasulov <145886003+mirsaidl@users.noreply.github.com>
2024-02-22 18:07:39 +0900

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,11 @@ import streamlit as st
 import pathlib
 import plotly.express as px
 import platform
+from fastai.learner import load_learner
+from pathlib import Path
+
+
+
 
 plt = platform.system()
 if plt == 'Linux' : pathlib.WindowsPath = pathlib.PosixPath
@@ -17,9 +22,10 @@ if file is not None:
     img  = PILImage.create(file)
     
     # model
-    model = load_learner('transport_model.pkl')
-    pred, pred_id, probs = model.predict(img)
-    
+    # Convert the model path to a string
+    model_path = str(Path('transport_model.pkl'))
+    # Load the learner
+    model = load_learner(model_path)
     # text
     st.success(f"Prediction:  {pred}")
     st.info(f"Probability: {probs[pred_id]*100:.1f}%")
